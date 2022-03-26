c05480b418419a84a3d60bd99012a2ae5e3027ec
Ritchie <flamener@hotmail.com>
2022-03-25 23:33:00 -0700

    non-error

--- bing-rewards/BingRewards/src/telegram.py
+++ bing-rewards/BingRewards/src/telegram.py
@@ -15,17 +15,10 @@ class TelegramMessenger():
         else:
             print(f"Boo! Telegram notification NOT sent, response code is: {resp} with response msg {resp.text}\n")
 
-<<<<<<< HEAD
     def send_reward_message(self, stats_str, run_hist_str, email):
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         message = f'\n Summary for {email} at : {current_time} \n\n' + \
         "\n".join(stats_str) + \
         f"\nRun Log: {[run_hist_str]}"
-=======
-    def send_reward_message(self, stats, email):
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        message = f'\n Summary for {email} at : {current_time} \n\n' + \
-        "\n".join(stats.stats_str)
->>>>>>> bfc4560bb03ca51f355af432b015877018b35292
 
         self.send_message(message)
